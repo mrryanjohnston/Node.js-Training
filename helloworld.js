5333b0e959450f3b10fb721a9ef51db260088767
Ryan Johnston <mrryanjohnston@gmail.com>
2011-04-23 09:03:11 -0400

    Hello World server first steps.

--- helloworld.js
+++ helloworld.js
@@ -5,8 +5,5 @@ var server = http.createServer(function(request, response) {
   response.writeHead(200, {
     'Content-Type': 'text/plain'
   });
-  response.write('Hello World!');
-  response.end();
-});
-
-server.listen(8000);
+  response.end('Hellow World!');
+}).listen(8000);
